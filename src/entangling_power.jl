f58dbd9796cbc613b96346d8747c744e0f0e28ad
Tomohiro <tomohiro_soejima@g.harvard.edu>
2024-10-02 11:21:14 -0400

    fixed bugs from promotion rules

--- src/entangling_power.jl
+++ src/entangling_power.jl
@@ -52,7 +52,8 @@ end
 
 function make_brick_wall_dense_matrix(mat, n_qubits, local_hilbert_space_dimension)
     dimension = local_hilbert_space_dimension^n_qubits
-    final_matrix = Matrix{ComplexF64}(I, (dimension, dimension))
+    T = eltype(mat)
+    final_matrix = Matrix{T}(I, (dimension, dimension))
     final_matrix = reshape(final_matrix, tuple((ones(Int, 2*n_qubits) * 2)...))
     @assert n_qubits % 2 == 0
     for ind in 1:2:n_qubits
@@ -69,13 +70,20 @@ end
 
 
 function diagonalize_brick_wall(n_qubits, local_hilbert_space_dimension, eu, gu)
-    mat = py"matA"(local_hilbert_space_dimension, eu, gu)
+    mat = (py"matA"(local_hilbert_space_dimension, eu, gu))
     mat_reshaped = reshape(mat, (2, 2, 2, 2))
 
     mat_big = make_brick_wall_dense_matrix(mat_reshaped, n_qubits, local_hilbert_space_dimension)
     return eigen(reshape(mat_big, (2^n_qubits, 2^n_qubits))), mat_big
 end
 
+function diagonalize_brick_wall_arnoldi(n_qubits, local_hilbert_space_dimension, eu, gu)
+    mat = (py"matA"(local_hilbert_space_dimension, eu, gu))
+    mat_reshaped = reshape(mat, (2, 2, 2, 2))
+
+    mat_big = reshape(make_brick_wall_dense_matrix(mat_reshaped, n_qubits, local_hilbert_space_dimension), (2^n_qubits, 2^n_qubits))
+    return eigsolve(mat_big, 4)
+end
 
 #########################################################################
 ################# Implement sparse version ##############################
@@ -86,15 +94,17 @@ struct NSiteVector{T, N} <: AbstractVector{T}
 end
 
 import VectorInterface: zerovector, scale, scale!, scale!!, scalartype, add, add!, add!!, inner
-Base.:+(vec1::NSiteVector{T, N}, vec2::NSiteVector{T, N}) where {T, N} = NSiteVector{T, N}(vec1.tensor + vec2.tensor)
-add(vec1::NSiteVector{T, N}, vec2::NSiteVector{T, N}, α::Number=1, β::Number=1) where {T, N} = β * vec1 + α * vec2
-# add(vec1::NSiteVector{T, N}, vec2::NSiteVector{T, N}, α::Number, β::Number) where {T, N} = α * vec1 + β * vec2 # why is this necessary at all?
-add!!(vec1::NSiteVector{T, N}, vec2::NSiteVector{T, N}, α::Number=1, β::Number=1) where {T, N} = add(vec1, vec2, α, β)
+# Base.:+(vec1::NSiteVector{T, N}, vec2::NSiteVector{T, N}) where {T, N} = NSiteVector{T, N}(vec1.tensor + vec2.tensor) # promote_rule(T, T) = UnionAll{} for some reason
+Base.:+(vec1::NSiteVector{T, N}, vec2::NSiteVector{S, N}) where {T, S, N} = NSiteVector{promote_type(T, S), N}(vec1.tensor + vec2.tensor)
+add(vec1::NSiteVector, vec2::NSiteVector, α::Number=1, β::Number=1) = β * vec1 + α * vec2
+# add(vec1::NSiteVector, vec2::NSiteVector, α::Number, β::Number) where  = α * vec1 + β * vec2 # why is this necessary at all?
+add!!(vec1::NSiteVector, vec2::NSiteVector, α::Number=1, β::Number=1) = add(vec1, vec2, α, β)
 # add!!(vec1::NSiteVector{T, N}, vec2::NSiteVector{T, N}, α::Number, β::Number) where {T, N} = add(vec1, vec2, α, β) #somehow necessary
 # add!!(vec1::NSiteVector{T, N}, vec2::NSiteVector{T, N}, α::Number) where {T, N} = add!!(vec1, vec2, α, 1)
 
-Base.:*(a :: Number, vec::NSiteVector{T, N}) where {T, N} = NSiteVector{T, N}(a * vec.tensor)
+Base.:*(a :: T, vec::NSiteVector{S, N}) where {T<:Number, S, N} = NSiteVector{promote_type(T, S), N}(a * vec.tensor)
 scale!!(v :: NSiteVector{T, N}, α::Number) where {T, N} = scale(v, α)
+scale!!(w :: NSiteVector{T, N}, v :: NSiteVector{T, N}, α::Number) where {T, N} = scale(v, α)
 scale(v :: NSiteVector{T, N}, α::Number) where {T, N} = α * v
 
 Base.size(vec::NSiteVector) = prod(size(vec.tensor))
@@ -134,4 +144,14 @@ function (bw :: BrickWall)(vec::NSiteVector{T, N}) where {T, N}
 
     final_matrix = contract(bw.mat, final_matrix, n_qubits, 1)
     return final_matrix
+end
+
+function diagonalize_brick_wall_arnoldi_matrix_free(n_qubits, local_hilbert_space_dimension, eu, gu)
+    d = local_hilbert_space_dimension
+    matA = reshape(py"matA"(d, eu, gu), (d, d, d, d))
+    T = eltype(matA) # for some reason, the code only works for complex numbers. Maybe we get complex eigenvalues during orthogonalization?
+    vec1 = NSiteVector{T, n_qubits}(rand(T, ((d*ones(Int, n_qubits))...)))
+    bw = BrickWall{T}(matA);
+
+    return eigsolve(bw, vec1, 4)
 end
