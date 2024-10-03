7efd26af2d65ec0c61e37089a2f44b5558502c96
Tomohiro <tomohiro_soejima@g.harvard.edu>
2024-10-02 21:49:43 -0400

    add resolution data

--- src/scripting.jl
+++ src/scripting.jl
@@ -1,12 +1,12 @@
 using NPZ
 
-function run_brick_wall_scan(n_qubit, res, local_hilbert_space_dimension=2, verbosity=0, save=true)
+function run_brick_wall_scan(n_qubit, resolution, local_hilbert_space_dimension=2, verbosity=0, save=true)
     if local_hilbert_space_dimension == 2
-        eu_list = range(1/2, 2/3, res)
-        gu_list = range(0.25, 0.75, res)
+        eu_list = range(1/2, 2/3, resolution)
+        gu_list = range(0.25, 0.75, resolution)
     elseif local_hilbert_space_dimension == 3
-        eu_list = range(0.7, 1, res)
-        gu_list = range(0.35, 0.65, res)
+        eu_list = range(0.7, 1, resolution)
+        gu_list = range(0.35, 0.65, reresolutions)
     end
     iter = Iterators.product(eu_list, gu_list)
 
@@ -24,9 +24,9 @@ function run_brick_wall_scan(n_qubit, res, local_hilbert_space_dimension=2, verb
     end
 
     if save
-        npzwrite("eigvals_n=$(n_qubit)_res_$(res)_d_$(local_hilbert_space_dimension).npy", second_largest_eigenvalue)
-        npzwrite("eulist_n=$(n_qubit)_res_$(res)_$(local_hilbert_space_dimension).npy", collect(eu_list))
-        npzwrite("gulist_n=$(n_qubit)_res_$(res)_$(local_hilbert_space_dimension).npy", collect(gu_list))
+        npzwrite("eigvals_n=$(n_qubit)_res_$(resolution)_d_$(local_hilbert_space_dimension).npy", second_largest_eigenvalue)
+        npzwrite("eulist_n=$(n_qubit)_res_$(resolution)_$(local_hilbert_space_dimension).npy", collect(eu_list))
+        npzwrite("gulist_n=$(n_qubit)_res_$(resolution)_$(local_hilbert_space_dimension).npy", collect(gu_list))
     end
 end
 
